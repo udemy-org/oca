8e7a97880306b83d9f88d94a4c546a57c3b3ed78
Pep Mendez <jmendez1@xtec.cat>
2016-04-27 22:06:20 +0200

    Seccion 3 over

--- src/es/smartcoding/oca/seccion4/Leccion_4_01.java
+++ src/es/smartcoding/oca/seccion4/Leccion_4_01.java
@@ -10,6 +10,22 @@ package es.smartcoding.oca.seccion4;
  * 
  *         En esta lección estudiaremos en detalle las diferentes partes de un
  *         método.
+ * 
+ *         La declaración de un método consta de varios elementos:
+ * 
+ *         1. Un modificador de acceso opcional.
+ * 
+ *         2. Uno o más especificadores opcionales.
+ * 
+ *         3. Un tipo de retorno obligatorio.
+ * 
+ *         4. Un nombre de método obligatorio.
+ * 
+ *         5. Una lista de parámetros entre parentesis que puede ser vacia.
+ * 
+ *         6. Una lista opcional de excepciones.
+ *         
+ *         7. Un bloque de código que puede ser un bloque vacio.
  *
  */
 public class Leccion_4_01 {
