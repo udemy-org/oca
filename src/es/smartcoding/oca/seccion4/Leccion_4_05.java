d9c7ca86f95625a1c3d86dbfc71bd0a59c05993d
Pep Mendez <jmendez1@xtec.cat>
2016-04-28 14:52:47 +0200

    leccion 4-05 (overloading) in progress

--- src/es/smartcoding/oca/seccion4/Leccion_4_05.java
+++ src/es/smartcoding/oca/seccion4/Leccion_4_05.java
@@ -9,7 +9,12 @@ package es.smartcoding.oca.seccion4;
  *         La sobrecarga de métodos
  * 
  *         En esta lección abordaremos el tema de la sobrecarga de métodos:
- *         métodos con el mismo nombre y una lista de parámetros diferente.
+ *         métodos con el mismo nombre y una lista de parámetros (signatura)
+ *         diferente.
+ * 
+ *         Ya hemos utilizado métodos sobrecargados sin darnos cuenta. Por
+ *         ejemplo System.out.println() es un método sobrecargado porque admite
+ *         virtualmente cualquier tipo de argumento.
  *
  */
 public class Leccion_4_05 {
