665700bf6e51ac17ea45062323a7f8ac56b73292
Pep Mendez <jmendez1@xtec.cat>
2016-05-03 14:59:18 +0200

    seccion6 in progress

--- src/es/smartcoding/oca/seccion5/Leccion_5_04.java
+++ src/es/smartcoding/oca/seccion5/Leccion_5_04.java
@@ -19,6 +19,11 @@ package es.smartcoding.oca.seccion5;
  *         referencia de una interfaz que la implemente ya sea directa o
  *         indirectamente.
  * 
+ *         Como ves, creamos un único objeto de MiClaseConcreta que después
+ *         asignamos a diferentes referencias y el código compila y funciona sin
+ *         necesidad de castings. Esto se conoce como polimorfismo, la habilidad
+ *         de un objeto de asignarse a diferentes referencias relacionadas.
+ * 
  */
 
 interface I1 {
@@ -39,6 +44,12 @@ class MiClaseConcreta extends ClaseBase {
 	public void f() {
 		System.out.println("en f()");
 	}
+}
+
+class C01 {
+}
+
+class C02 {
 }
 
 public class Leccion_5_04 {
@@ -71,7 +82,47 @@ public class Leccion_5_04 {
 		 */
 		I1 i1 = cc;
 		i1.f();
-		((I2)i1).g();
+		// i1.g(); // No compila
+		/*
+		 * Cuando se asigna un objeto a una referencia, sólo los miembros
+		 * disponibles en ese tipo de referencia estan disponibles sin hacer
+		 * cast.
+		 * 
+		 * Por ejemplo, si asignamos un objeto a una referencia de tipo Object,
+		 * sólo estarán disponibles los métodos de la clase Object. Pero el
+		 * objeto en sí no ha cambiado. Más tarde pordemos hacer una cast a
+		 * MiClaseConcreta y seguiremos teniendo acceso a los miembros de esta
+		 * clase.
+		 */
+		Object object = cc;
+		cc = (MiClaseConcreta) object;
+		cc.f();
+		cc.g();
+
+		/*
+		 * Casting tiene una serie de reglas:
+		 * 
+		 * 1. Convertir un objeto de una subclase a una superclase no necesita
+		 * un casting explícito.
+		 * 
+		 * 2. Convertir un objeto de una superclase a un subclase SÍ que
+		 * necesita un casting específico.
+		 * 
+		 * 3. El compilador no permitirá conversiones imposibles.
+		 * 
+		 * 4. Incluso si el código compila, si el objeto a convertir no es de
+		 * hecho una instancia de la otra clase, en tiempo de ejecució lanzará
+		 * una excepción de tipo java.lang.ClassCastException.
+		 */
+		/*
+		 * Esta conversión compila porque Object y String forman parte de la
+		 * jerarquía pero lanza una excepción java.lang.ClassCastException en
+		 * tiempo de ejecución.
+		 */
+		String strObject = (String) object;
+		Number number = new Integer(1);
+		// String strNumber = (String) number; // No compila, conversión
+		// imposible
 	}
 
 }
