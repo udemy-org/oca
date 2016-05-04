a4eec0668cb2c13622775e4862a81d2abfcb7ae5
Pep Mendez <jmendez1@xtec.cat>
2016-05-04 10:06:11 +0200

    Leccion 6-03 en progreso

--- src/es/smartcoding/oca/seccion6/Leccion_6_03.java
+++ src/es/smartcoding/oca/seccion6/Leccion_6_03.java
@@ -10,7 +10,29 @@ package es.smartcoding.oca.seccion6;
  * 
  *         En esta lección estudiaremos cuáles son las excepciones más comunes
  *         que debes conocer durante el examen oficial.
- *
+ * 
+ *         El examen OCA questionará tu conocimiento sobre tres tipos de
+ *         excepciones: excepciones no comprobadas o excepciones en tiempo de
+ *         ejecución, excepciones comprobadas y errores.
+ * 
+ *         Las excepciones runtime extienden RuntimeException no tienen que ser
+ *         ni tratadas ni declaradas. Pueden ser lanzadas por el programador o
+ *         por la Java Virtual Machine y estas son las más comunes:
+ *        
+ *         1. ArithmeticException 
+ *         
+ *         2. ArrayIndexOutOfBoundsException
+ *         
+ *         3. ClassCastException
+ *         
+ *         4. IllegalArgumentException
+ *         
+ *         5. NullPointerException
+ *         
+ *         6. NumerFormatException
+ *         
+ *         
+ *         
  */
 public class Leccion_6_03 {
 
