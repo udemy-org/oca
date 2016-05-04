643b9322ed4ae6b10893baa8eac60285643f4e5a
Pep Mendez <jmendez1@xtec.cat>
2016-05-04 11:42:06 +0200

    Leccion 6-03 over

--- src/es/smartcoding/oca/seccion6/Leccion_6_03.java
+++ src/es/smartcoding/oca/seccion6/Leccion_6_03.java
@@ -20,16 +20,36 @@ package es.smartcoding.oca.seccion6;
  *         por la Java Virtual Machine y estas son las más comunes:
  * 
  *         1. ArithmeticException
+ *         
+ *         int i = 1 / 0;
  * 
  *         2. ArrayIndexOutOfBoundsException
+ *         
+ *         int[] v = new int[] {1,2,3};
+ *         int k = v[3];
  * 
  *         3. ClassCastException
+ *         
+ *         String str = "Alfa";
+ *         Object obj = str;
+ *         Integer i = (Integer) obj;
  * 
  *         4. IllegalArgumentException
+ *         
+ *         void f(int e) {
+ *            if(e < 0) {
+ *            	throw new IllegalArgumentException("El argumento no puede ser negativo");
+ *            }
+ *         }
  * 
  *         5. NullPointerException
+ *         
+ *         String s = null;
+ *         s.length();
  * 
  *         6. NumberFormatException
+ *         
+ *         int i = Integer.parseInt("alpha");
  * 
  *         Las excepciones comprobadas con Exception's que no son
  *         RuntimeException's. Pueden ser lanzadas por el programador o por la
