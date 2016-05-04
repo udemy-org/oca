001b628576f125ea06fede8297fe31ec480d6a34
Pep Mendez <jmendez1@xtec.cat>
2016-05-04 10:15:17 +0200

    Leccion 6-03 almost there

--- src/es/smartcoding/oca/seccion6/Leccion_6_03.java
+++ src/es/smartcoding/oca/seccion6/Leccion_6_03.java
@@ -18,21 +18,38 @@ package es.smartcoding.oca.seccion6;
  *         Las excepciones runtime extienden RuntimeException no tienen que ser
  *         ni tratadas ni declaradas. Pueden ser lanzadas por el programador o
  *         por la Java Virtual Machine y estas son las más comunes:
- *        
- *         1. ArithmeticException 
- *         
+ * 
+ *         1. ArithmeticException
+ * 
  *         2. ArrayIndexOutOfBoundsException
- *         
+ * 
  *         3. ClassCastException
- *         
+ * 
  *         4. IllegalArgumentException
- *         
+ * 
  *         5. NullPointerException
+ * 
+ *         6. NumberFormatException
+ * 
+ *         Las excepciones comprobadas con Exception's que no son
+ *         RuntimeException's. Pueden ser lanzadas por el programador o por la
+ *         Java Virtual Machine y estas son las más comunes:
+ * 
+ *         1. FileNotFoundException
+ * 
+ *         2. IOException
+ * 
+ *         Los errores son instancias de la clase java.lang.Error. Pueden ser
+ *         lanzadas sólo por la Java Virtual Machine y no deberían ser ni
+ *         tratadas ni declaradas. Los errores son raros pero quizan veas alguno
+ *         de estos:
  *         
- *         6. NumerFormatException
+ *         1. ExceptionInInitizationError
  *         
+ *         2. StackOverflowError
  *         
- *         
+ *         3. NoClassDefFoundError
+ * 
  */
 public class Leccion_6_03 {
 
