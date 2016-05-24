f1f291b3326d05cd7095d4f68ff07baa946b68c1
Pep Mendez <jmendez1@xtec.cat>
2016-05-24 07:20:15 +0200

    --ammend

--- src/es/smartcoding/oca/seccion3/Leccion_3_06.java
+++ src/es/smartcoding/oca/seccion3/Leccion_3_06.java
@@ -41,14 +41,16 @@ public class Leccion_3_06 {
 		 * La clase java.time.LocalTime contiene sólo una hora.
 		 * 
 		 * La clase java.time.LocalDateTime contiene una fecha y una hora.
+		 * 
+		 * No se usan los constructores para crear nuevas instancias sino los métodos estáticos of() y now().
+		 * 
 		 */
-		System.out.println(LocalDate.now());
-		System.out.println(LocalTime.now());
-		System.out.println(LocalDateTime.now());
+		System.out.println("LocalDate.now(): " + LocalDate.now());
+		System.out.println("LocalTime.now(): " + LocalTime.now());
+		System.out.println("LocalDateTime.now(): " + LocalDateTime.now());
 
 		/*
-		 * Estas dos fechas son equivalentes. Si bien el tipo enumerado Month no
-		 * forma parte del examen OCA.
+		 * Estas dos fechas son equivalentes. Si bien el tipo enumerado Month no forma parte del examen OCA.
 		 */
 		LocalDate fecha1 = LocalDate.of(2015, 1, 20);
 		LocalDate fecha3 = LocalDate.of(2015, Month.JANUARY, 20);
@@ -66,8 +68,7 @@ public class Leccion_3_06 {
 		LocalTime hora3 = LocalTime.of(6, 15, 30, 200);
 
 		// También se puede combinar la fecha y la hora en un mismo objeto. SIEMPRE a través de métodos estáticos.
-		LocalDateTime fechaHora1 = LocalDateTime.of(2015, Month.JANUARY, 20, 6,
-				15, 30);
+		LocalDateTime fechaHora1 = LocalDateTime.of(2015, Month.JANUARY, 20, 6, 15, 30);
 		LocalDateTime fechaHora2 = LocalDateTime.of(fecha1, hora1);
 		
 		/*
@@ -77,84 +78,82 @@ public class Leccion_3_06 {
 		 * recomendados (deprecated), y los meses empiezan en 0 lo que añade confusión.
 		 */
 		Date date1 = new Date();
-		System.out.println(date1);
+		System.out.println("date1: " + date1);
 		Calendar calendar1 = Calendar.getInstance();
 		calendar1.set(2016, 4, 21);
-		System.out.println(calendar1);
+		System.out.println("calendar1: " + calendar1);
 		Date date2 = calendar1.getTime();
-		System.out.println(date2);
+		System.out.println("date2: " + date2);
 		Calendar calendar2 = new GregorianCalendar(2016, Calendar.APRIL, 21);
 		Date date3 = calendar2.getTime();
 		// Date date4 = new Date(2015, Calendar.JANUARY, 12); // Deprecated
+		
 		// Manipular fechas con las nuevas clases de Java 8 es fácil.
 		LocalDate date5 = LocalDate.of(2014, Month.JANUARY, 20);
 		// 2014-01-20
-		System.out.println(date5);
+		System.out.println("date5: " + date5);
 		date5 = date5.plusDays(2);
 		// 2014-01-22
-		System.out.println(date5);
+		System.out.println("date5 plus 2 days: " + date5);
 		date5 = date5.plusWeeks(1);
 		// 2014-01-29
-		System.out.println(date5);
+		System.out.println("date5 plus 1 week: " + date5);
 		date5 = date5.plusMonths(1);
 		// 2014-02-28
-		System.out.println(date5);
+		System.out.println("date5 plus 1 month: " + date5);
 		date5 = date5.plusYears(5);
 		// 2019-02-28
-		System.out.println(date5);
+		System.out.println("date5 plus 5 years: " + date5);
 		// También tenemos la familia de métodos minus...
 		date5 = date5.minus(Period.ofDays(3));
-		System.out.println(date5);
+		System.out.println("date5 minus 3 days: " + date5);
 		date5 = date5.minus(1, ChronoUnit.YEARS);
-		System.out.println(date5);
+		System.out.println("date5 minus 1 year: " + date5);
 		// La clase LocalDate es inmutable, por lo tanto cuidado! este código no tiene ningún efecto:
 		date5.minusDays(1);
-		System.out.println(date5);
+		System.out.println("date5 minus 1 day: " + date5);
 		// En Java 8 Java define la clase Period. Hay cinco maneras de definir un período:
 		
-		// Estos son algunos ejemplos de periodos: Cada año
+		// Estos son algunos ejemplos de periodos (años, meses y días): Cada año
 		Period period1 = Period.ofYears(1);
-		System.out.println(period1);
+		System.out.println("period1: " + period1);
 		// Cada trimestre
 		Period period2 = Period.ofMonths(3);
 		System.out.println("period2: " + period2);
 		// Cada tres semanas
 		Period period3 = Period.ofWeeks(3);
-		System.out.println(period3);
+		System.out.println("period3: " + period3);
 		// Día sí y dia no, cada dos días
 		Period period4 = Period.ofDays(2);
-		System.out.println(period4);
+		System.out.println("peridod4: " + period4);
 		// Cada año y siete días
 		Period period5 = Period.of(1, 0, 7);
-		System.out.println(period5);
+		System.out.println("period5: " + period5);
 		/*
 		 * Los períodos no se pueden encadenar al contrario que las fechas. Este
-		 * período es de siete días contrariamente a lo que parece. A eso se
-		 * debe el warning que puedes ver.
+		 * período es de siete días contrariamente a lo que parece porque el método es static.
 		 */
 		Period period6 = Period.ofYears(1).ofWeeks(1);
-		System.out.println(period6);
+		System.out.println("period6: " + period6);
 		/*
 		 * Si bien un período comprende un día o más, las instancias de la clase
-		 * Duration pueden contener el número de días, horas, minutos, segundos
-		 * y nanosegundos.
+		 * Duration pueden contener el número de días, horas, minutos, segundos y nanosegundos.
 		 * 
-		 * Duration no está presente en el examen pero te puede ser útil en tu
-		 * día a día.
+		 * Ni Duration ni Instant (un instante) están presentes en el examen pero te puede ser útil en tu día a día.
 		 */
 		Duration duration1 = Duration.of(10, ChronoUnit.MINUTES);
-		System.out.println(duration1);
+		System.out.println("duration1: " + duration1);
 		Instant instant1 = Instant.now();
-		System.out.println(instant1);
+		System.out.println("instant1: " + instant1);
 		/*
 		 * ISO es una estándar para las fechas.
 		 */
 		LocalDate date = LocalDate.of(2020, Month.JANUARY, 20);
 		LocalTime time = LocalTime.of(11, 12, 34);
 		LocalDateTime dateTime = LocalDateTime.of(date, time);
-		System.out.println(date.format(DateTimeFormatter.ISO_LOCAL_DATE));
-		System.out.println(time.format(DateTimeFormatter.ISO_LOCAL_TIME));
-		System.out.println(dateTime
+		System.out.println("format date: " + date.format(DateTimeFormatter.ISO_LOCAL_DATE));
+		System.out.println("format time: " + time.format(DateTimeFormatter.ISO_LOCAL_TIME));
+		System.out.println("format datetime: " + dateTime
 				.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
 		/*
 		 * La clase DateTimeFormatter de Java 8 viene a ser como la clase
@@ -163,19 +162,19 @@ public class Leccion_3_06 {
 		DateTimeFormatter shortDateTime = DateTimeFormatter
 				.ofLocalizedDate(FormatStyle.SHORT);
 		// 1/20/20
-		System.out.println(shortDateTime.format(dateTime));
+		System.out.println("dateTime short date time format 1: " + shortDateTime.format(dateTime));
 		// 1/20/20
-		System.out.println(shortDateTime.format(date));
+		System.out.println("date short date time format 1: " + shortDateTime.format(date));
 		// UnsupportedTemporalTypeException
 		// System.out.println(shortDateTime.format(time));
 		/*
 		 * Este código es equivalente al anterior. Se puede utilizar uno u otro
 		 * indistintamente.
 		 */
-		System.out.println(date.format(shortDateTime));
+		System.out.println("date short date time format 2: " + date.format(shortDateTime));
 		// UnsupportedTemporalTypeException
 		// System.out.println(time.format(shortDateTime));
-		System.out.println(dateTime.format(shortDateTime));
+		System.out.println("dateTime short date time format 2: " + dateTime.format(shortDateTime));
 		/*
 		 * Si por algún motivo no queremos utilizar los formatos predeterminados
 		 * (SHORT, MEDIUM, LONG o FULL), podemos crear nuestros propios formatos
@@ -196,7 +195,7 @@ public class Leccion_3_06 {
 		 */
 		DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter
 				.ofPattern("MMMM dd, yyyy, hh:mm");
-		System.out.println(dateTime.format(dateTimeFormatter1));
+		System.out.println("dateTime dateTimeFormatter1: " + dateTime.format(dateTimeFormatter1));
 		/*
 		 * Por último, nos queda por tratar el tema de la conversión de una cadena en un objeto fecha.
 		 * 
@@ -207,9 +206,9 @@ public class Leccion_3_06 {
 		LocalDate localDate = LocalDate.parse("01 02 2015", dateTimeFormatter2);
 		LocalTime localTime = LocalTime.parse("11:22");
 		// 2015-01-02
-		System.out.println(localDate);
+		System.out.println("localDate: " + localDate);
 		// 11:22
-		System.out.println(localTime);
+		System.out.println("localTime: " + localTime);
 	}
 
 }
